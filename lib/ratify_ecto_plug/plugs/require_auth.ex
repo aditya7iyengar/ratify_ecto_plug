77f8e7f513ec7e939aeacbbe9888e6b4547ac7c8
Adi Iyengar <aditya7iyengar@gmail.com>
2017-05-01 22:04:33 -0400

    Add plugs:
    - Add plug for setting up resource.
    - Add plug for require auth.

---
+++ lib/ratify_ecto_plug/plugs/require_auth.ex
@@ -0,0 +1,23 @@
+defmodule Ratify.Ecto.Plug.Plugs.RequireAuth do
+  def init(params) do
+    params = Enum.into(params, %{})
+    %{handler: build_handler_tuple(params)}
+  end
+
+  def call(conn, params) do
+    case conn.assigns[:user] do
+      nil ->
+        {module, function} = Map.get(params, :handler)
+        apply(module, function, [conn])
+      _ -> conn
+    end
+  end
+
+  defp build_handler_tuple(%{handler: handler_module}) do
+    {handler_module, :handle_unauthenticated}
+  end
+
+  defp build_handler_tuple(_) do
+    {Ratify.Handlers.AuthHandler, :handle_unauthenticated}
+  end
+end
