1cf54b9e27c779ed29fce85d4434bc9197a85e53
Adi Iyengar <aditya7iyengar@gmail.com>
2017-05-01 23:48:39 -0400

    Fix configuration key

--- lib/ratify_ecto_plug.ex
+++ lib/ratify_ecto_plug.ex
@@ -3,26 +3,26 @@ defmodule Ratify.Ecto.Plug do
   A module that provides Simple authentication for Ecto and Plug based applications.
 
   ## Configuration
-      config :ratify, Ratify.Ecto.Plug,
+      config :ratify_ecto_plug, Ratify.Ecto.Plug,
         user_struct: MyApp.User,
         user_repo: MyApp.Repo
 
   """
 
-  unless Application.get_env(:ratify, Ratify.Ecto.Plug) do
+  unless Application.get_env(:ratify_ecto_plug, Ratify.Ecto.Plug) do
     raise "Ratify.Ecto.Plug is not configured"
   end
 
-  unless Keyword.get(Application.get_env(:ratify, Ratify.Ecto.Plug), :user_struct) do
+  unless Keyword.get(Application.get_env(:ratify_ecto_plug, Ratify.Ecto.Plug), :user_struct) do
     raise "Ratify.Ecto.Plug requires a user_struct"
   end
 
-  unless Keyword.get(Application.get_env(:ratify, Ratify.Ecto.Plug), :user_repo) do
+  unless Keyword.get(Application.get_env(:ratify_ecto_plug, Ratify.Ecto.Plug), :user_repo) do
     raise "Ratify.Ecto.Plug requires a user_repo"
   end
 
   @doc false
-  def config, do: Application.get_env(:ratify, Ratify.Ecto.Plug)
+  def config, do: Application.get_env(:ratify_ecto_plug, Ratify.Ecto.Plug)
 
   @doc false
   def config(key), do: config() |> Keyword.get(key)
