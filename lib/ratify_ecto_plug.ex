c3c0883fb79f0d2ec77bfd6df5d8852c184d2785
Adi Iyengar <aditya7iyengar@gmail.com>
2017-05-01 22:03:29 -0400

    Add ratify ecto plug:
    - Module doc with example of setting up configs
    - Test for config setup

---
+++ lib/ratify_ecto_plug.ex
@@ -0,0 +1,29 @@
+defmodule Ratify.Ecto.Plug do
+  @moduledoc """
+  A module that provides Simple authentication for Ecto and Plug based applications.
+
+  ## Configuration
+      config :ratify, Ratify.Ecto.Plug,
+        user_struct: MyApp.User,
+        user_repo: MyApp.Repo
+
+  """
+
+  unless Application.get_env(:ratify, Ratify.Ecto.Plug) do
+    raise "Ratify.Ecto.Plug is not configured"
+  end
+
+  unless Keyword.get(Application.get_env(:ratify, Ratify.Ecto.Plug), :user_struct) do
+    raise "Ratify.Ecto.Plug requires a user_struct"
+  end
+
+  unless Keyword.get(Application.get_env(:ratify, Ratify.Ecto.Plug), :user_repo) do
+    raise "Ratify.Ecto.Plug requires a user_repo"
+  end
+
+  @doc false
+  def config, do: Application.get_env(:ratify, Ratify.Ecto.Plug)
+
+  @doc false
+  def config(key), do: config() |> Keyword.get(key)
+end
